f4fbedd8e603c69e31cbbcbc9edde49bf1ce371d
admin <129410356+usernamesadmins@users.noreply.github.com>
2024-09-28 16:05:34 +0800

    Create agenda.js

---
+++ agenda.js
@@ -0,0 +1,15 @@
+var guding = $response.body;
+guding = JSON.parse(guding);
+guding.unlockExpiryMobile = 148204937166;
+guding.licenseStatusMobile = 2;
+guding.licenseStatus = 2;
+guding.informUserOfPendingBonus = false;
+guding.hasLifetimeUnlock = false;
+guding.freeTrialStatus = 1;
+guding.isSubscribed = true;
+guding.unlockExpiry = 148204937166;
+guding.isSubscribedMobile = true;
+guding.error = 0;
+guding.universalPurchaseAvailable = false;
+guding = JSON.stringify(guding);
+$done({ body: guding });
