fc090f9e027c236881f22639a982f09e3b5f401c
admin <129410356+usernamesadmins@users.noreply.github.com>
2024-10-06 11:21:34 +0800

    Update nmqds.js

--- nmqds.js
+++ nmqds.js
@@ -3,10 +3,7 @@ var objc = JSON.parse($response.body);
     objc = {
   "code" : 200,
   "data" : {
-    "create_time" : "2023-07-27 15:47:17",
     "vip_expire" : "2099-12-31 18:10:32",
-    "cause" : "成功",
-    "flag" : "permanent",
   },
   "msg" : "ok",
   "t" : "2024-04-03 12:21:23"
