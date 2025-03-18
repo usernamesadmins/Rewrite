0719fa81b1043660305f27ebfaaa24fef4d5b879
admin <129410356+usernamesadmins@users.noreply.github.com>
2025-03-18 21:44:51 +0800

    Update and rename bcAn.js to pt.js

---
+++ pt.js
@@ -0,0 +1,28 @@
+const py996 = {};
+const py997 = JSON.parse(typeof $response != "undefined" && $response.body || null);
+
+const name = "pixelmator_photo_pro_access";
+const appid = "pixelmator_photo_lifetime_v1_pro_offer";
+
+  
+if (typeof $response == "undefined") {
+  delete $request.headers["x-revenuecat-etag"];
+  delete $request.headers["X-RevenueCat-ETag"];
+
+  py996.headers = $request.headers;
+} else if (py997 && py997.subscriber) {
+  data = {
+    "expires_date": "9999-09-09T09:09:09Z",
+    "original_purchase_date": "2023-02-23T02:33:33Z",
+    "purchase_date": "2023-02-23T02:33:33Z",
+    "ownership_type": "PURCHASED",
+    "store": "app_store"
+  };
+
+  py997.subscriber.subscriptions[(appid)] = data
+  py997.subscriber.entitlements[(name)] = JSON.parse(JSON.stringify(data));
+  py997.subscriber.entitlements[(name)].product_identifier = (appid);
+  py996.body = JSON.stringify(py997);
+}
+
+$done(py996);
